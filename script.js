b810fdf933c9f72be0bbbea7926ee3c9957cb6c2
J Akkermans <99044668@mydavinci.nl>
2020-10-17 11:03:01 +0200

    works as requested -return function.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-var num1 = 0;
-var num2 = 1;
-var store;
-var x = 0;
+var num1 = 0;       //Begin number1 = 0;
+var num2 = 1;       //Begin number2 = 0;
+var storage;        //Used to store the calcluations.
 
-function fibonacci(num) {
-    
-        for (i = 0; i < num; i++) {
-            store = num1 + num2;
-            
-            num1 = num2;
-            num2 = store;
-
-
-            console.log(store)
-            
+function fibonacciCalc(num) {
+        for (i = 0; i < num; i++) {     //loop trough the times user wants fibonachi calculations to be done.
+            num1 = num2;                // num 1  = 2 (1)
+            num2 = storage;             //num 2 = storage
+            storage = num1 + num2;      //storage = num1 + num2; | Which will change due to the loop doing this 20x. 
+            console.log(num2);
+            document.write(storage + "<br>")
         }
-    return num2;
+    
 }
 
-document.write("Fibonacci(20): " + fibonacci(20) + "<br>");
 
+// Edit (20) to any number to get fibonacci to calcluate to that certain number =). 
+fibonacciCalc(20) 
