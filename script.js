f93d573c0163d703e6c57cf59f26705a91169c71
J Akkermans <99044668@mydavinci.nl>
2020-10-28 11:44:47 +0100

    fix

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var num1 = 0;       //Begin number1 = 0;
 var num2 = 1;       //Begin number2 = 1;
-var storage;        //Used to store the calcluations.
+var storage = 0;        //Used to store the calcluations.
 
 function fibonacciCalc(num) {
         for (i = 0; i < num; i++) {     //loop trough the times user wants fibonachi calculations to be done.
+            document.write(storage + "<br>") //Print to dom + <br> 
             storage = num1 + num2;      //storage = num1 + num2; | Which will change due to the loop doing this 20x. 
             num1 = num2;                // num 1  = 2 (1)
             num2 = storage;             //num 2 = storage
-            document.write(storage + "<br>") //Print to dom + <br> 
         }
     
 }
